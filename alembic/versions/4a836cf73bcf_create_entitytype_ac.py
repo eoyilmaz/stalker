c57e725d3779bf32ea9acd666249aacc0ca00388
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2013-05-15 18:01:29 +0300

    * SOM:   * New: Added alembic migrations which adds the ``accepts_references`` column     to ``EntityTypes`` table and ``original_name`` to the ``Links`` table.

---
+++ alembic/versions/4a836cf73bcf_create_entitytype_ac.py
@@ -0,0 +1,24 @@
+"""create EntityType.accepts_references
+
+Revision ID: 4a836cf73bcf
+Revises: None
+Create Date: 2013-05-15 16:27:05.983849
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '4a836cf73bcf'
+down_revision = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.add_column('EntityTypes', sa.Column('accepts_references', sa.Boolean))
+    op.add_column('Links', sa.Column('original_filename', sa.String(256)))
+
+def downgrade():
+    # no drop column in SQLite so this will not work for SQLite databases
+    op.drop_column('EntityTypes', 'accepts_references')
+    op.drop_column('Links', 'original_filename')
