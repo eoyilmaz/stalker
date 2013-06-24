c63117b675e500c97a0bbc23fc8f953ef58dceb2
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2013-06-24 12:38:14 +0300

    * **Update:** Removed **Sequence.lead_id** use **Task.reponsible** instead. * **Update:** Added alembic revision for addition of Tasks.responsible_id   and removal of Sequences.lead_id.

---
+++ alembic/versions/101a789e38ad_created_task_respons.py
@@ -0,0 +1,32 @@
+"""Created Task.responsible attribute
+
+Revision ID: 101a789e38ad
+Revises: 59092d41175c
+Create Date: 2013-06-24 12:32:04.852386
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '101a789e38ad'
+down_revision = '59092d41175c'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    try:
+        op.drop_column('Sequences', u'lead_id')
+        op.add_column('Tasks',
+                      sa.Column('responsible_id', sa.Integer(), nullable=True))
+    except sa.exc.OperationalError:
+        pass
+
+
+def downgrade():
+    try:
+        op.drop_column('Tasks', 'responsible_id')
+        op.add_column('Sequences',
+                      sa.Column(u'lead_id', sa.INTEGER(), nullable=True))
+    except sa.exc.OperationalError:
+        pass
