8a7f1f465f9e6d9c13a650873bbf81d75681ad0d
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2014-06-05 17:13:35 +0300

    the debug message in db.create_alembic_table() method is only showing when really creating the table

--- stalker/db/__init__.py
+++ stalker/db/__init__.py
@@ -172,10 +172,9 @@ def create_alembic_table():
         ins = table.insert().values(version_num=version_num)
         DBSession.connection().execute(ins)
         DBSession.commit()
+        logger.debug('alembic_version table is created and initialized')
     else:
         logger.debug('alembic_version table is already there!')
-
-    logger.debug('alembic_version table is created and initialized')
 
 
 def __create_admin__():
