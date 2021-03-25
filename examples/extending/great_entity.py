3d2eecb3f4c1edc7f9f5f5fa6c73eca5feb241e3
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2021-03-25 09:12:06 +0300

    Removed all the unnecessary Copyright statements from every single source file.

--- examples/extending/great_entity.py
+++ examples/extending/great_entity.py
@@ -1,20 +1,4 @@
 # -*- coding: utf-8 -*-
-# Stalker a Production Asset Management System
-# Copyright (C) 2009-2018 Erkan Ozgur Yilmaz
-#
-# This file is part of Stalker.
-#
-# Stalker is free software: you can redistribute it and/or modify
-# it under the terms of the Lesser GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License.
-#
-# Stalker is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# Lesser GNU General Public License for more details.
-#
-# You should have received a copy of the Lesser GNU General Public License
-# along with Stalker.  If not, see <http://www.gnu.org/licenses/>
 """
 In this example we are going to extend stalker with a new entity type, which
 is also mixed in with a :class:`stalker.models.mixins.ReferenceMixin`.
--- tests/models/test_group.py
+++ tests/models/test_group.py
@@ -1,20 +1,4 @@
 # -*- coding: utf-8 -*-
-# Stalker a Production Asset Management System
-# Copyright (C) 2009-2018 Erkan Ozgur Yilmaz
-#
-# This file is part of Stalker.
-#
-# Stalker is free software: you can redistribute it and/or modify
-# it under the terms of the Lesser GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License.
-#
-# Stalker is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# Lesser GNU General Public License for more details.
-#
-# You should have received a copy of the Lesser GNU General Public License
-# along with Stalker.  If not, see <http://www.gnu.org/licenses/>
 
 
 import unittest
