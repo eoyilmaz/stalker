9015256c10eec02cb71aa3beb70c5d44e5170422
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2024-10-28 14:46:56 +0000

    [#79] Removed encoding tag from empty `__init__.py` files.

--- stalker/__init__.py
+++ stalker/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Stalker is a Production Asset Management System (ProdAM) designed for
-Animation and VFX Studios.
+"""Stalker is a Production Asset Management (ProdAM) designed for Animation/VFX Studios.
 
 See docs for more information.
 """
@@ -9,19 +8,19 @@ from stalker import config, log
 
 defaults = config.Config()
 
-from stalker.models.auth import (
-    Group,
-    Permission,
-    User,
-    LocalSession,
-    Role,
-    AuthenticationLog,
-)
 from stalker.models.asset import Asset
-from stalker.models.budget import Budget, BudgetEntry, Good, PriceList, Invoice, Payment
+from stalker.models.auth import (
+    AuthenticationLog,
+    Group,
+    LocalSession,
+    Permission,
+    Role,
+    User,
+)
+from stalker.models.budget import Budget, BudgetEntry, Good, Invoice, Payment, PriceList
 from stalker.models.client import Client, ClientUser
 from stalker.models.department import Department, DepartmentUser
-from stalker.models.entity import SimpleEntity, Entity, EntityGroup
+from stalker.models.entity import Entity, EntityGroup, SimpleEntity
 from stalker.models.format import ImageFormat
 from stalker.models.link import Link
 from stalker.models.message import Message
@@ -42,24 +41,24 @@ from stalker.models.mixins import (
 from stalker.models.note import Note
 from stalker.models.project import (
     Project,
-    ProjectUser,
     ProjectClient,
     ProjectRepository,
+    ProjectUser,
 )
-from stalker.models.review import Review, Daily, DailyLink
 from stalker.models.repository import Repository
+from stalker.models.review import Daily, DailyLink, Review
 from stalker.models.scene import Scene
 from stalker.models.schedulers import SchedulerBase, TaskJugglerScheduler
 from stalker.models.sequence import Sequence
 from stalker.models.shot import Shot
 from stalker.models.status import Status, StatusList
 from stalker.models.structure import Structure
-from stalker.models.studio import Studio, WorkingHours, Vacation
+from stalker.models.studio import Studio, Vacation, WorkingHours
 from stalker.models.tag import Tag
-from stalker.models.task import TimeLog, Task, TaskDependency
+from stalker.models.task import Task, TaskDependency, TimeLog
 from stalker.models.template import FilenameTemplate
 from stalker.models.ticket import Ticket, TicketLog
-from stalker.models.type import Type, EntityType
+from stalker.models.type import EntityType, Type
 from stalker.models.version import Version
 from stalker.models.wiki import Page
 
