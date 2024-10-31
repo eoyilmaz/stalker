a004d2ecfb5a546d40659acf6739ec2710d85dc4
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2024-10-31 16:38:17 +0000

    [#79] Added debug output for `tests.utils.run_db_command()`.

--- tests/utils.py
+++ tests/utils.py
@@ -94,6 +94,16 @@ def run_db_command(
         if stderr != "":
             stderr_buffer.append(stderr)
 
+    logger.debug("STDOUT BUFFER")
+    logger.debug("=============")
+    for line in stdout_buffer:
+        logger.debug(line)
+
+    logger.debug("STDERR BUFFER")
+    logger.debug("=============")
+    for line in stderr_buffer:
+        logger.debug(line)
+
     return stdout_buffer, stderr_buffer
 
 
